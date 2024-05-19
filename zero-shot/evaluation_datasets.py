ceb54018164f06668394d5120a6a2c864bbb282c
maxdoesch <doesch.maximilian@gmail.com>
2024-05-19 16:45:06 +0200

    remove weird frame conversion (frames = frames.astype(np.float32) / 255.0 * 2.0 - 1.0) in evaluation_dataset.py

--- zero-shot/evaluation_datasets.py
+++ zero-shot/evaluation_datasets.py
@@ -513,7 +513,6 @@ def create_davis_dataset(
       if resolution is not None and resolution != frames.shape[1:3]:
         frames = resize_video(frames, resolution)
 
-    frames = frames.astype(np.float32)
     target_points = davis_points_dataset[video_name]['points']
     target_occ = davis_points_dataset[video_name]['occluded']
     target_points = target_points * np.array([frames.shape[2], frames.shape[1]])
