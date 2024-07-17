05537a995e74e81a54a3622e30a7eb0cb6d0c00d
maxdoesch <doesch.maximilian@gmail.com>
2024-07-17 23:33:08 +0200

    delete extract_diffusion_feature file

--- extract_diffusion_features.py
+++
@@ -1,5 +0,0 @@
-from algorithms.feature_extraction_loading import extract_diffusion_features
-
-
-if __name__ == '__main__':
-    extract_diffusion_features(input_dataset_paths={'davis': 'tapvid_davis/'}, output_dataset_path='features/', diffusion_model_path='text-to-video-ms-1.7b/', restrict_ncomponents=True, n_components=10)
